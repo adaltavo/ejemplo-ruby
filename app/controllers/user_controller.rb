4955e8b1e397862768e3eeaea082394f05b85c97
Amaralp <paola.amaral@sinneg.com>
2017-02-02 11:51:22 -0700

    cambios1

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,11 @@
 class UserController < ApplicationController
   def index
   end
+  def new
+  	puts entro
+  end
+  def update
+  end
+  def delete
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   get 'user/index'
 
+  resources :users
+
   get 'invoice/index'
 
   get 'invoicedetail/index'
 
   get 'item/index'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
