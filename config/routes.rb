9efd9e028e471d4daa8634c99a813d4293c42f8d
adaltavo <tavo050894@gmail.com>
2017-02-03 08:18:23 -0700

    cosas

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,19 @@ Rails.application.routes.draw do
 
   resources :users
 
-  get 'invoice/index'
+  get 'custom/route' => 'user#index'
 
   get 'invoicedetail/index'
 
   get 'item/index'
 
-
+#GET	/photos	photos#index	display a list of all photos
+#GET	/photos/new	photos#new	return an HTML form for creating a new photo
+#POST	/photos	photos#create	create a new photo
+#GET	/photos/:id	photos#show	display a specific photo
+#GET	/photos/:id/edit	photos#edit	return an HTML form for editing a photo
+#PATCH/PUT	/photos/:id	photos#update	update a specific photo
+#DELETE	/photos/:id	photos#destroy	delete a specific photo
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
