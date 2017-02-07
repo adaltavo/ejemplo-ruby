970328e22c6db2625b9e3624280e57f579f7f261
Amaralp <paola.amaral@sinneg.com>
2017-02-07 12:57:29 -0700

    cambiousuario

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,16 @@
 class UserController < ApplicationController
   def index
+
   end
   def new
-  	
+    @user=User.new
+  end
+  def create
+    @user=User.create(name:params[:user][:name],
+                    password:params[:user][:password],
+                    correo:params[:user][:correo],
+                    rfc:params[:user][:rfc]);
+
   end
   def update
   end
